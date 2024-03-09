62d95e287a73381db0c48cd4a75097ded77c1dc3
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-09 09:57:42 +0600

    rearranged

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
 
 let notes = [
     {
@@ -24,6 +19,8 @@ let notes = [
     }
 ]
 
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -31,14 +28,17 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json())
 app.use(requestLogger)
-
-
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -92,7 +92,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
-
 
 app.use(unknownEndpoint)
 
