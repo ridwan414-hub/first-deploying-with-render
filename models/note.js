f24c0edc10b7a0f96527961c2971e1702fc74b36
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-13 19:23:17 +0600

    updated

--- models/note.js
+++ models/note.js
@@ -14,8 +14,13 @@ mongoose.connect(url)
     })
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    important: Boolean
 })
 
 noteSchema.set('toJSON', {
